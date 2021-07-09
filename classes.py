7ab2dd17cfde0c8320459271293506440da489ef
Mateusz Domański <matstadom@gmail.com>
2021-07-09 13:03:35 +0200

    classes payment and debts to model the behaviour

---
+++ classes.py
@@ -0,0 +1,37 @@
+class Debt:
+    def __init__(self, id, title, to_whom, how_much, date, paid=False, paid_off=0):
+        self.id = id
+        self.title = title
+        self.to_whom = to_whom
+        self.how_much = int(how_much)
+        self.date = date
+        self.paid = paid
+        self.paid_off = paid_off
+
+    def to_dict(self):
+        dictionary = {
+            "id": self.id,
+            "title": self.title,
+            "to_whom": self.to_whom,
+            "how_much": self.how_much,
+            "date": self.date,
+            "paid": self.paid,
+            "paid_off": self.paid_off
+        }
+        return dictionary
+
+class Payment:
+    def __init__(self, id, reciever, amount, date):
+        self.id = id
+        self.reciever = reciever
+        self.amount = int(amount)
+        self.date = date
+    
+    def to_dict(self):
+        dictionary = {
+            "id": self.id,
+            "reciever": self.reciever,
+            "amount": self.amount,
+            "date": self.date
+        }
+        return dictionary
